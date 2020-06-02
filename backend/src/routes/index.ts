0a613ab57155f67cb382b01e9b2e32f3b455f6e5
italosnunes <46608041+italosnunes@users.noreply.github.com>
2020-06-02 19:32:13 -0300

    :raised_hands: backend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,16 @@
+/* eslint-disable camelcase */
 import { Router } from 'express';
 
+import PointsController from '../controller/PointsController';
+import ItemsController from '../controller/ItemsController';
+
 const routes = Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => response.json({ message: 'Helo World' }));
+routes.get('/items', itemsController.index);
 
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 export default routes;
